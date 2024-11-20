50d0fdb318e06226f09b72cfb8c112046df4544f
tsyoon <yoontsy2000@gmail.com>
2024-11-20 19:06:38 +0900

    Redesigned portfolio

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,83 @@
 import NavMenu from "@/components/NavMenu";
-import BioSection from "@/components/BioSection";
-import Timeline from "@/components/Timeline";
 
 export default function Home() {
+  const pathname = usePathname();
   return (
     <div className="flex flex-col min-h-screen">
-      <div className="bg-black p-7"/>
-      <header>
-          <NavMenu/>
-      </header>
-      <main className="w-1/2 mx-auto">
-        <section className="py-16 mx-auto">
-          <div className="text-center">
-            <h1 className="text-2xl">Taesung Yoon | 윤태성</h1>
+      <NavMenu />
+      <section className="mx-auto px-6 pt-[120px]">
+        <h1 className="p-16 text-5xl font-bold ">hi, i am taesung!</h1>
+        <div className="grid grid-cols-2">
+          <div>
+            <h2 className="p-16 text-5xl font-bold ">about me!</h2>
+            <p className="px-16 text-2xl font-semibold">
+              Hi, I’m a software engineer passionate about creating seamless and accessible user experiences. With a background in Computer Science from Boston University and experience in front-end development, I combine technical expertise with a focus on human-centered design. I thrive at the intersection of code and creativity, building solutions that empower users.
+            </p>
           </div>
-        </section>
-        <BioSection/>
-        
-        <section className="container">
-          <h2 className="font-semibold mx-auto">Timeline</h2>
-          <Timeline/>
-        </section>
-        <p className="font-semibold">Contact me</p>
-      </main>
-      <div className="w-full fixed bottom-0 bg-black p-7 flex space-x-4">
-        {/* <div className="bg-white p-5"/> */}
+          <div>
+          <h2 className="p-16 text-5xl font-bold ">my interests!</h2>
+            <p className="px-16 text-2xl font-semibold">
+              <ul>
+                <li>🖥️ frontend engineering</li>
+                <li>🧑‍💻 human-computer interaction</li>
+                <li>🧑‍💻 photography</li>
+                <li>🎥 films</li>
+                <li>📚 books</li>
+              </ul>
+            </p>
+          </div>
+        </div>
+      </section>
+      <section className="px-6 pt-[60px]">
+        <h2 className="p-16 text-5xl font-bold">timeline</h2>
+        <div className="flex justify-between">
+          <p className="px-16 text-2xl font-semibold">Graduated with a Bachelor's in Computer Science</p>
+          <p className="px-16 text-2xl font-semibold">May 2024</p>
+        </div>
+        <div className="flex justify-between">
+          <p className="px-16 text-2xl font-semibold">Resumed Studies at Boston University</p>
+          <p className="px-16 text-2xl font-semibold">Sep 2023 - May 2024</p>
+        </div>
+        <div className="flex justify-between">
+          <p className="px-16 text-2xl font-semibold">Military Service</p>
+          <p className="px-16 text-2xl font-semibold">Jan 2022 - July 2023</p>
+        </div>
+        <div className="flex justify-between">
+          <p className="px-16 text-2xl font-semibold">Internship at Boston University: Software and Applications Lab</p>
+          <p className="px-16 text-2xl font-semibold">May 2021 - Aug 2021</p>
+        </div>
+        <div className="flex justify-between">
+          <p className="px-16 text-2xl font-semibold">Boston University: Spark!</p>
+          <p className="px-16 text-2xl font-semibold">Sep 2020 - Dec 2020</p>
+        </div>
+        <div className="flex justify-between">
+          <p className="px-16 text-2xl font-semibold">Internship at SuperTree</p>
+          <p className="px-16 text-2xl font-semibold">May 2020 - Aug 2020</p>
+        </div>
+      </section>
+      <section className="px-6 pt-[60px]">
+        <h2 className="p-16 text-5xl font-bold">current and past projects</h2>
+        <div className="flex justify-between">
+          <p className="px-16 text-2xl font-semibold">Portfolio Website</p>
+          <p className="px-16 text-2xl font-semibold">2024 - Current</p>
+        </div>
+        <div className="flex justify-between">
+          <p className="px-16 text-2xl font-semibold">WhyPhi (Application Tracking System)</p>
+          <p className="px-16 text-2xl font-semibold">2020 - 2024</p>
+        </div>
+
+        <div className="flex justify-between">
+          <p className="px-16 text-2xl font-semibold">Smart Placement Operator</p>
+          <p className="px-16 text-2xl font-semibold">2024</p>
+        </div>
+
+        <div className="flex justify-between">
+          <p className="px-16 text-2xl font-semibold">HackNYU Winner: Down2Dine</p>
+          <p className="px-16 text-2xl font-semibold">2019</p>
+        </div>
+
+      </section>
+      <div className="mx-auto">
       </div>
     </div>
   );
--- components/NavMenu.tsx
+++ components/NavMenu.tsx
@@ -1,9 +1,9 @@
 export default function NavMenu() {
   return (
-    <nav className="md:w-1/2 mx-auto z-2">
-      <div className="container mx-auto flex justify-between items-center p-4 border-b-4">
+    <nav className="w-full fixed border-b-2 bg-white">
+      <div className="container mx-auto flex justify-between items-center p-4">
         <a href="#" className="font-bold text-2xl">
-          <span>taesung</span>
+          <span>home</span>
         </a>
         <ul className="flex space-x-4">
           <li>
